2a6910824e5fb7db179e5783ce61e307fe96e1b3
AlexAntonio29 <alexantonioangel29@gmail.com>
2025-04-10 12:29:50 -0600

    Nueva aplicacion Felicitacion AndroidStudio

--- Android Studio/app/src/main/java/com/app/appholamundo/MainActivity.java
+++ Android Studio/app/src/main/java/com/app/appholamundo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.app.appholamundo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bPush;
+    TextView tvF;
+    TextView tvName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bPush.findViewById(R.id.bPush);
+        tvF.findViewById(R.id.tvF);
+        tvName.findViewById(R.id.tvName);
+        bPush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bPush.setVisibility(View.INVISIBLE);
+                tvF.setVisibility(View.VISIBLE);
+                tvName.setVisibility(View.VISIBLE);
+
+            }
         });
     }
 }
